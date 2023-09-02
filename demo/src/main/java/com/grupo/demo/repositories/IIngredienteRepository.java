b4ee9530f55c351f4895a6eefe0be4a762034f85
PabloDL <pablo.delafore@hotmail.com>
2023-09-02 16:34:34 -0300

    generados los proto
    generados los proto
    repository incompletos

---
+++ demo/src/main/java/com/grupo/demo/repositories/IIngredienteRepository.java
@@ -0,0 +1,23 @@
+package com.grupo.demo.repositories;
+
+import com.grupo.demo.entities.Ingrediente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Repository("ingredienteRepository")
+public interface IIngredienteRepository extends JpaRepository<Ingrediente, Serializable>{
+
+        @SuppressWarnings("unchecked")
+        public Ingrediente save(Ingrediente ingrediente);
+
+        @Query(nativeQuery=true,value="Select * from ingrediente")
+        public List<Categoria> traerTodos();
+
+        public abstract Ingrediente findByDescripcion(String descripcion);
+        public abstract Ingrediente findById(int id);
+    }
+}
